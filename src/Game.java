cb58d911f8eec5929e260e0dd93ffde38ff5631c
aeturkaslan <aeturkaslan@hotmail.com>
2023-02-25 19:02:55 +0300

    first commit

---
+++ src/Game.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Game {
+	Player player;
+	Location location;
+	
+	public void login() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome To The Adventure Game!");
+		System.out.println("Please Enter Your Name: ");
+		String playerName = scan.nextLine();
+		player = new Player(playerName);
+	}
+
+}
